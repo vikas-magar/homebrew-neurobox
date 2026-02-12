ebe9d1c1fde7eb1d3bded51e86f7ed068cc31fa7
Vikas Magar <119872471+vikas-magar@users.noreply.github.com>
2026-02-12 21:06:39 +0530

    Update homepage and install method for Neurobox
    updated the formula

--- Formula/neurobox.rb
+++ Formula/neurobox.rb
@@ -1,20 +1,17 @@
 class Neurobox < Formula
   desc "OCI-Compatible Transactional Sandbox"
-  homepage "https://github.com/vikas-magar/Neurobox"
+  homepage "https://nbx.vercel.app"
   version "0.18.0"
 
   url "https://github.com/vikas-magar/homebrew-neurobox/releases/download/v#{version}/neurobox-macos.tar.gz"
   sha256 "6eec8123af91559f44b8e3a0a171bca06452a7a4a9455d7a1717079f7aebaa76"
 
   def install
-    bin.install "neurobox" => "nbx"
-  end
-
-  def post_install
-    binary = "#{bin}/nbx"
+    binary = "neurobox"
     chmod 0755, binary
     system "xattr", "-d", "com.apple.quarantine", binary rescue nil
     system "codesign", "--force", "-s", "-", binary rescue nil
+    bin.install "neurobox" => "nbx"
   end
 
   test do
