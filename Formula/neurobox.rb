2bb759fedf7912d94ba9bc847a9a04d621c09907
Vikas Magar <119872471+vikas-magar@users.noreply.github.com>
2026-02-12 20:27:14 +0530

    {formulaName} {version}
    Created by Github Action from vikas-magar/Neurobox

--- Formula/neurobox.rb
+++ Formula/neurobox.rb
@@ -1,10 +1,10 @@
 class Neurobox < Formula
   desc "OCI-Compatible Transactional Sandbox"
   homepage "https://github.com/vikas-magar/Neurobox"
-  version "0.16.0"
+  version "0.18.0"
 
-  url "https://github.com/vikas-magar/homebrew-neurobox/releases/download/v#{version}/neurobox-macos.tar.gz"
-  sha256 "2ef6cfc0583ebc69f0bd7128c32ee277de0c0a50c2a7094a346de55dffafefa7"
+  url "https://github.com/vikas-magar/homebrew-neurobox/releases/download/v0.18.0/neurobox-macos.tar.gz"
+  sha256 "6eec8123af91559f44b8e3a0a171bca06452a7a4a9455d7a1717079f7aebaa76"
 
   def install
     bin.install "neurobox" => "nbx"
